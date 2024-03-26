22851823c1c86487d440d9bb34c898e22af737d6
LauraVEkris <laura.vanekris@student.hu.nl>
2024-03-26 11:56:49 +0100

    chatGPT help please

--- checkcode.php
+++ checkcode.php
@@ -1,12 +1,15 @@
 <?php
-if (!isset($_POST["name"])) {
+if (!isset($_POST["name"]) || empty($_POST["name"])) {
     echo ("Name is empty");
+    exit;
 }
-if (!filter_input(INPUT_POST, "code", FILTER_VALIDATE_INT)) {
-    echo ("Code is empty");
+if (!isset($_POST["code"]) || empty($_POST["code"]) || !filter_var($_POST["code"], FILTER_VALIDATE_INT)) {
+    echo ("Code is empty or invalid");
+    exit;
 }
+
 $name = $_POST["name"];
-$code = filter_input(INPUT_POST, "code", FILTER_VALIDATE_INT);
+$code = $_POST["code"];
 
 $str = file_get_contents('/var/www/db.json');
 $json = json_decode($str, true); // decode the JSON into an associative array
@@ -16,24 +19,28 @@ $username = $json['Username'];
 $password = $json['Password'];
 $database = "Lobby";
 
-//Create connection
+// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-
 if ($conn->connect_error) {
     mysqli_close($conn);
     die ("connection error");
 }
 
+// Prepare and bind the statement
+$stmt = $conn->prepare("SELECT Code FROM Rooms WHERE code = ? LIMIT 1");
+$stmt->bind_param("i", $code);
+$stmt->execute();
+$stmt->store_result();
 
-$result = $conn->execute_query("SELECT Code FROM Rooms WHERE code = ? LIMIT 1", [$code]);
-if ($result->num_rows == 1) {
-    echo "<script type='text/javascript' src='cookie.js'>setCookie('" . $_POST["name"] . "', '" . $_POST["code"] . "');</script>";
+if ($stmt->num_rows == 1) {
+    echo "<script type='text/javascript' src='cookie.js'></script>";
+    echo "<script type='text/javascript'>setCookie('" . $_POST["name"] . "', '" . $_POST["code"] . "');</script>";
 } else {
     mysqli_close($conn);
     header("Location: /index.php");
-    die("room does not exist");
+    die("Room does not exist");
 }
 
 mysqli_close($conn);
