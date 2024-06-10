9dcd0c650c60bc1b8bedb54c2d3aff052a4e58d3
happpy24 <vivianspeijer@gmail.com>
2024-06-10 13:31:04 +0200

    Update unitybridge.js

--- site/game/unitybridge.js
+++ site/game/unitybridge.js
@@ -1,36 +1,19 @@
-// This variable will store the Unity instance
-var unityInstance;
-
-// Function to retrieve the value from session storage and send it to Unity
-function RetrieveAndSendValuesToUnity() {
-    // Retrieve the value of the 'name' variable from session storage
-    var name = sessionStorage.getItem('name');
-
-    // Retrieve the value of the 'code' variable from cookies
-    var code = getCookie('code');
-
-    // Send the 'name' value to Unity
-    if (unityInstance) {
-        unityInstance.SendMessage('JavascriptHook', 'ReceiveName', name);
-        unityInstance.SendMessage('JavascriptHook', 'ReceiveCode', code);
-    } else {
-        console.error("Unity instance not found. Unable to send message.");
+function UnityLoaded(unityInstance) {
+    // Retrieve session storage value
+    const sessionName = sessionStorage.getItem('name');
+    if (sessionName) {
+        unityInstance.SendMessage('WebGLReceiver', 'ReceiveName', sessionName);
     }
-}
 
-// Function to get a cookie value by name
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
+    // Retrieve cookie value
+    function getCookie(name) {
+        let value = `; ${document.cookie}`;
+        let parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
 
-// This function is called when Unity is fully loaded
-function UnityLoaded(instance) {
-    // Store the Unity instance for future use
-    unityInstance = instance;
-
-    // Call the function to retrieve and send the values to Unity
-    RetrieveAndSendValuesToUnity();
+    const cookieCode = getCookie('code');
+    if (cookieCode) {
+        unityInstance.SendMessage('WebGLReceiver', 'ReceiveCode', cookieCode);
+    }
 }
